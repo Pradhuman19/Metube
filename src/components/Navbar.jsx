6acddbcdc2ea995eef43d2a7775f8d5f85c39cbd
Pradhuman Gupta <pradhumngupta14@gmail.com>
2024-10-25 19:27:49 +0530

    nseachbar centered

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,12 +9,13 @@ const Navbar = () =>
    direction="row"
    alignItems= "center"
    p={2}
-   sx={{position: 'sticky',background:'#000', top: 0, justifyContent: 'space-between'}}
+   sx={{position: 'sticky',background:'#000', top: 0
+     }}
   >
   <Link to="/" style={{display: 'flex' ,alignItems:'centre'}}>
     <img src={logo} alt="logo" height={45} />
   </Link>
-  <SearchBar />
+  <span style={{marginLeft:'auto',marginRight:'auto'}}><SearchBar /></span>
   </Stack>
   );
 
