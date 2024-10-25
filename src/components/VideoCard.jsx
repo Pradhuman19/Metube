346ad161fa9990687bc135b15ffcb61b4876e7f5
Pradhuman Gupta <pradhumngupta14@gmail.com>
2024-10-25 19:37:33 +0530

    videocard changed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,7 +6,7 @@ import { demoThumbnailUrl,demoVideoUrl,demoVideoTitle,demoChannelUrl,demoChannel
 
 const VideoCard = ({video: {id:{videoId},snippet}}) => {
   
- return ( <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
+ return ( <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 2 , border:'1px solid gray'}}>
   <Link to={videoId ? `/video/${videoId}` : `/video/demoVideoUrl` }>
     <CardMedia 
     image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} 
@@ -14,7 +14,7 @@ const VideoCard = ({video: {id:{videoId},snippet}}) => {
     sx={{ width: { xs: '100%', sm: '358px', md: '320px'}, height: 180 }} 
     />
   </Link>
-  <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
+  <CardContent sx={{ backgroundColor: 'black', height: '80px' }}>
     <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
       <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
         {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
